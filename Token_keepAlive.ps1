7f69ba8beabfd8c956d54bc1ab8ace00f9119c39
Yan <yanzhenxin123456@hotmail.com>
2025-06-17 23:39:15 +0800

    feat: 添加README和Token保活脚本

---
+++ Token_keepAlive.ps1
@@ -0,0 +1,23 @@
+﻿##
+# 油菜花系统配置
+##
+# 需保活的Token 
+# 多个Token保活示例: $YchToken = '1111', '2222'
+$YchToken = '114514'
+$YchKeepAliveUri = 'https://pw.gzych.vip/ykb_huiyuan/api/v1/Member/GetMemberStoredValue'
+
+foreach ( $Token in $YchToken ) {  
+    $Headers = @{
+        'Authorization' = $Token
+    }
+
+    try {
+        $KeepAliveRequest = Invoke-WebRequest -Uri $YchKeepAliveUri -Headers $Headers
+        Write-Host -ForegroundColor Green "Token有效"
+    } catch {
+        Write-Host -ForegroundColor Red "Token无效,需重新获取"
+        continue
+    }
+}
+
+exit
